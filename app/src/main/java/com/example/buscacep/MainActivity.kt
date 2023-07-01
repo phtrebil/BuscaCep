39f66d182db385c17f9a7f5cbf902498724acc5d
phtrebil <phptrebil@gmail.com>
2023-07-01 11:24:34 -0300

    ocultando informação de endereço, finalizando projeto utilizando call

--- app/src/main/java/com/example/buscacep/MainActivity.kt
+++ app/src/main/java/com/example/buscacep/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.buscacep
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.widget.Toast
 import com.example.buscacep.databinding.ActivityMainBinding
 import com.example.buscacep.model.Endereco
@@ -15,30 +18,44 @@ class MainActivity : AppCompatActivity() {
     val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    var endereco: Endereco? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.botaoDeBusca.setOnClickListener {
-            val call = EnderecoRepository().cepService.buscaEndereco(binding.cepEditText.text.toString())
-                call.enqueue(object: Callback<Endereco>{
-                    override fun onResponse(call: Call<Endereco>, response: Response<Endereco>) {
-                        if (response.isSuccessful) {
-                            val endereco = response.body()
-                            binding.rua.text = endereco?.rua
-                            binding.bairro.text = endereco?.bairro
-                            binding.cidade.text = endereco?.cidade
-                            binding.estado.text = endereco?.estado
-                        } else {
-                            Toast.makeText(this@MainActivity, "Endereço não encontrado", Toast.LENGTH_SHORT).show()
-                        }
-                    }
+        // Oculta o layout de informações de endereço inicialmente
+        binding.constraintLayoutInfoEndereco.visibility = GONE
 
-                    override fun onFailure(call: Call<Endereco>, t: Throwable) {
+        binding.botaoBuscar.setOnClickListener {
+            val call = EnderecoRepository().cepService.buscaEndereco(binding.cep.text.toString())
+
+            // Faz uma chamada assíncrona à API para buscar o endereço com base no CEP fornecido
+            call.enqueue(object : Callback<Endereco> {
+                override fun onResponse(call: Call<Endereco>, response: Response<Endereco>) {
+                    if (response.isSuccessful) {
+                        // Exibe o layout de informações de endereço quando a resposta é bem-sucedida
+                        binding.constraintLayoutInfoEndereco.visibility = VISIBLE
+                        endereco = response.body()
+                        binding.rua.text = endereco?.rua
+                        binding.bairro.text = endereco?.bairro
+                        binding.cidade.text = endereco?.cidade
+                        binding.estado.text = endereco?.estado
+                    } else {
+                        // Oculta o layout de informações de endereço se a resposta não for bem-sucedida
+                        binding.constraintLayoutInfoEndereco.visibility = GONE
                         Toast.makeText(this@MainActivity, "Endereço não encontrado", Toast.LENGTH_SHORT).show()
                     }
+                }
 
-                })
+                override fun onFailure(call: Call<Endereco>, t: Throwable) {
+                    // Oculta o layout de informações de endereço em caso de falha na chamada
+                    binding.constraintLayoutInfoEndereco.visibility = GONE
+                    Toast.makeText(this@MainActivity, "Endereço não encontrado", Toast.LENGTH_SHORT).show()
+                }
+
+            })
         }
     }
 }
